8af7cb939d0120902cbb1fed378969ff7051d13b
Iromedes <iromedess@gmail.com>
2026-05-30 12:07:13 -0300

    Configura ambiente de desenvolvimento e remove segurança

--- src/main/java/com/example/tecnologia/model/Transacao.java
+++ src/main/java/com/example/tecnologia/model/Transacao.java
@@ -16,6 +16,9 @@ public class Transacao {
     private BigDecimal valor;
     private LocalDateTime data;
 
+    // Campo adicionado para a sua regra 30/30/40
+    private String categoria;
+
     // Construtor vazio (obrigatório para o JPA)
     public Transacao() {
     }
@@ -41,12 +44,20 @@ public class Transacao {
         this.valor = valor;
     }
 
-    // Métodos para a data que você adicionou
     public LocalDateTime getData() {
         return data;
     }
 
     public void setData(LocalDateTime data) {
         this.data = data;
+    }
+
+    // Novos métodos Getter e Setter para a categoria
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
     }
 }
