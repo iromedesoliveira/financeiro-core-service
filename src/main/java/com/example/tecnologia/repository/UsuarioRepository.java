26283a6c27572d09a063a8da50eb061e4b02ce4c
Iromedes <iromedess@gmail.com>
2026-05-30 21:56:48 -0300

    feat: initial project structure with Spring Boot, H2, and Oracle JDBC driver

--- src/main/java/com/example/tecnologia/repository/UsuarioRepository.java
+++ src/main/java/com/example/tecnologia/repository/UsuarioRepository.java
@@ -3,8 +3,11 @@ package com.example.tecnologia.repository;
 import com.example.tecnologia.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // O JpaRepository já nos dá métodos como findById, save, etc.
+    // Agora você tem a funcionalidade de buscar por email no seu repositório
+    // oficial
+    Optional<Usuario> findByEmail(String email);
 }
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-MERGE INTO tb_usuario t
-USING (SELECT 1 AS id FROM dual) s
-ON (t.id = s.id)
-WHEN NOT MATCHED THEN
-  INSERT (id, nome, email, senha) VALUES (1, 'Iromedes', 'iromedes@email.com', '123456');
